ad5f0fc737edae6c1a023c8cea95ed9c29d41faa
Grayson112233 <graysonpike@gmail.com>
2020-10-27 02:36:02 -0500

    Invite links, members, sarcastic comments

--- bot.py
+++ bot.py
@@ -1,36 +1,74 @@
 from config import get_config
 import discord
+from random import randrange
 
 
 class Client(discord.Client):
 
+    def __init__(self, config=None):
+        discord.Client.__init__(self)
+        self.config = config
+
     def print_guilds(self):
         print("Guilds Connected:")
-        for guild in self.guilds:
-            print(guild.name)
+        for guild_id in self.config["guild_order"]:
+            for guild in self.guilds:
+                if guild.id == guild_id:
+                    print(str(guild.id) + " " + guild.name)
 
     async def on_ready(self):
         print('Connected as', self.user)
-        self.print_guilds()
+        # self.print_guilds()
+        # await self.print_guild_members(759959782022184980)
 
     async def on_message(self, message):
         # don't respond to ourselves
         if message.author == self.user:
             return
 
+        # If Jared says something in the CTMUN Social Server, reply a sarcastic comment
+        if message.guild.id == 759959782022184980 and message.author.id == 690732698234257408:
+            print("Replying sarcastic message to Jared...")
+            await self.send_sarcastic_message(message.channel)
+
+        # If Grayson says something in the Bot Testing Server, reply a sarcastic comment
+        if message.guild.id == 760945039328673822 and message.author.id == 175063777048395776:
+            print("Replying sarcastic message to Grayson...")
+            await self.send_sarcastic_message(message.channel)
+
+        # Just because
         if message.content == 'ping':
-            print("Guild ID:")
-            print(message.guild.id)
             await message.channel.send('pong')
+            
+
+    async def create_invite_links(self):
+        for guild_id in self.config["guild_order"]:
+            for guild in self.guilds:
+                if guild.id == guild_id:
+                    for channel in guild.channels:
+                        if channel.name == "general" and isinstance(channel, discord.TextChannel):
+                            invite = await channel.create_invite()
+                            print(invite.url)
 
 
-def get_token():
-    return get_config()['discord_token']
+    async def send_sarcastic_message(self, channel):
+        message = self.config['sarcastic_messages'][randrange(0, len(self.config['sarcastic_messages']))]
+        await channel.send(message)
+
+
+    async def print_guild_members(self, guild_id):
+        for guild in self.guilds:
+            if guild.id == guild_id:
+                async for member in guild.fetch_members():
+                    print(str(member.id) + " " + member.display_name)
+
+
 
 
 def main():
-    token = get_token()
-    client = Client()
+    config = get_config()
+    token = config['discord_token']
+    client = Client(config=config)
     client.run(token)
 
 
